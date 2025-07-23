0b9fdc637d94b56d47d2b0bb6292f2d89ca4d141
Shivansh <shivansh832.be22@chitkara.edu.in>
2025-07-23 15:26:58 +0530

    Lecture-15 23-07-2025

---
+++ L15_CHANGESET_23072025/CHANGELOG.md
@@ -0,0 +1,13 @@
+# calculator_functionality
+
+## 1.0.2
+
+### Patch Changes
+
+- Subtraction Added
+
+## 1.0.1
+
+### Patch Changes
+
+- addition
---
+++ L15_CHANGESET_23072025/index.js
@@ -0,0 +1,11 @@
+function add(){
+    console.log("Addition Functionality");
+    return;
+}
+
+function sub(){
+    console.log("Subtraction Functionality");
+    return;
+}
+
+module.exports = {add , sub};
